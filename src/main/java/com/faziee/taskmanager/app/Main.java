508c1e31e98c63befc5a4753023c28273a4575b1
Faziee <144025566+Faziee@users.noreply.github.com>
2025-12-26 23:10:38 +0100

    Add FlatLaf dependency

--- src/main/java/com/faziee/taskmanager/app/Main.java
+++ src/main/java/com/faziee/taskmanager/app/Main.java
@@ -2,6 +2,8 @@ package com.faziee.taskmanager.app;
 import com.faziee.taskmanager.core.TaskManager;
 import com.faziee.taskmanager.storage.TaskRepository;
 import com.faziee.taskmanager.ui.TaskManagerApp;
+import com.formdev.flatlaf.FlatLightLaf;
+
 
 import javax.swing.*;
 
@@ -9,6 +11,15 @@ public class Main
 {
     public static void main(String[] args)
     {
+        try
+        {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        }
+        catch (Exception e)
+        {
+            System.err.println("Failed to initialize FlatLaf");
+        }
+
         TaskManager taskManager = new TaskManager();
         TaskRepository repository = new TaskRepository("tasks.json");
 
